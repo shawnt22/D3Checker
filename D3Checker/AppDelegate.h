887daacaaebfb2367c01058af264d7bc38b43726
滕 松 <ShawnTeng@toumatoMacBook-Pro.local>
2012-06-01 22:40:06 +0800

    Initial Commit

---
+++ D3Checker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  D3Checker
+//
+//  Created by 松 滕 on 12-6-1.
+//  Copyright (c) 2012年 Sohu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
