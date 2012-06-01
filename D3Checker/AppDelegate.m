94ef1e4d2e6118fbbccde02c5dcac281965d837a
滕 松 <ShawnTeng@toumatoMacBook-Pro.local>
2012-06-01 22:42:46 +0800

    工程搭建

--- D3Checker/AppDelegate.m
+++ D3Checker/AppDelegate.m
@@ -3,7 +3,7 @@
 //  D3Checker
 //
 //  Created by 松 滕 on 12-6-1.
-//  Copyright (c) 2012年 Sohu.com. All rights reserved.
+//  Copyright (c) 2012年 shawnt22@gmail.com . All rights reserved.
 //
 
 #import "AppDelegate.h"
